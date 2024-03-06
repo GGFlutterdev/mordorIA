4ba40e0c73a6b7678423f8e7dc66e0abb0e4207b
GGFlutterdev <luigi.allocca12345@live.it>
2024-03-06 14:37:06 +0100

    Improved datasheet, added more info

--- datasheet.py
+++ datasheet.py
@@ -4,11 +4,7 @@ from math import log
 
 datasheet_bp = Blueprint('datasheet_bp', __name__)
 
-@datasheet_bp.route('/datasheet')
-def datasheet():
-    tokenizer = current_app.tokenizer
-    corpus = current_app.corpus
-    
+def create_plot_data(tokenizer, corpus):
     # Tokenizza il corpus di testo con il tokenizer
     encoded_corpus = tokenizer.encode(corpus)
 
@@ -27,12 +23,24 @@ def datasheet():
     frequencies = [freq for _, freq in sorted_words]
     log_ranks = [log(rank) for rank in ranks]
     log_frequencies = [log(freq) for freq in frequencies]
+    
+    # Estrai i nomi delle parole
+    words = [word for word, _ in sorted_words]
 
-    # Passa i dati del grafico al template HTML
-    plot_data = {
+    return {
         'log_ranks': log_ranks,
-        'log_frequencies': log_frequencies
+        'log_frequencies': log_frequencies,
+        'word_names': words  # Aggiungi i nomi delle parole ai dati del grafico
     }
+
+@datasheet_bp.route('/datasheet')
+def datasheet():
+    tokenizer = current_app.tokenizer
+    corpus = current_app.corpus
+
+    # Ottieni i dati del grafico
+    plot_data = create_plot_data(tokenizer, corpus)
+    
 
     # Restituisci il template HTML con i dati del grafico
     return render_template('datasheet.html', plot_data=plot_data)
