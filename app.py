e5b425013922bd5d47338ccbfe8f1be791aacb93
GGFlutterdev <luigi.allocca12345@live.it>
2024-03-12 21:59:08 +0100

    Implemented word correction

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from tokenizers.pre_tokenizers import Whitespace
 from flask import Flask, render_template, request
 from tokenizers import Tokenizer
-from tokenizers.models import BPE
-from tokenizers.trainers import BpeTrainer
 from datasheet import datasheet_bp
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+import editdistance
 import nltk
 
 app = Flask(__name__)
@@ -29,6 +27,28 @@ stop_words = set(stopwords.words('english'))
 # Inizializza il lemmatizzatore
 lemmatizer = WordNetLemmatizer()
 
+def find_correct_words(words_separated_by_space):
+    # Lista per memorizzare le parole corrette
+    correct_words = []
+    
+    for word in words_separated_by_space:
+        found = False
+        min_distance = float('inf')
+        closest_word = None
+        for token in app.tokenizer.get_vocab():
+            
+            distance = editdistance.eval(word, token)
+            if distance == 0:
+                correct_words.append(word)
+                found = True
+                break
+            if distance < min_distance:
+                min_distance = distance
+                closest_word = token
+        if closest_word and found == False :
+            correct_words.append(closest_word)
+    return correct_words
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -40,17 +60,26 @@ def submit():
         user_input = request.form['user_input']
 
         encoded_text = app.tokenizer.encode(user_input)
-        # Restituisci i token al template HTML
         tokenized_text = encoded_text.tokens
+
+        words_separed_by_space = user_input.split()
+        # Trova le parole corrette utilizzando la distanza di edit
+        correct_words = find_correct_words(words_separed_by_space)
+        correct_words_text = ' '.join(correct_words)
+
+        encoded_correct_text = app.tokenizer.encode(correct_words_text)
+        # Restituisci i token al template HTML
+        tokenized_correct_text = encoded_correct_text.tokens
         # Identificazione stopwords
-        stopwords_removed = [word for word in tokenized_text if word.lower() in stop_words]
+        stopwords_removed = [word for word in tokenized_correct_text if word.lower() in stop_words]
         # Rimozione delle stopwords
-        cleaned_text_no_stopwords = [word for word in tokenized_text if word.lower() not in stop_words]
+        cleaned_text_no_stopwords = [word for word in tokenized_correct_text if word.lower() not in stop_words]
         # Lemmatizza il testo
         lemmatized_text = [lemmatizer.lemmatize(word) for word in cleaned_text_no_stopwords]
 
         return render_template('index.html',
                                tokens=tokenized_text,
+                               corrected_tokens = tokenized_correct_text,
                                tokens_no_stopwords = cleaned_text_no_stopwords,
                                lemmatized_text = lemmatized_text,
                                stopwords_removed = stopwords_removed
